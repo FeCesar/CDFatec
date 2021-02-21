1a02dd565790dbd8d5488013f5e610aee917e8b8
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-20 22:15:06 -0300

    Configurando o bulma / Icon

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt_br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Responsive -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Icon -->
+    <link rel="shortcut icon" href="./public/images/logo.png" type="image/x-icon" />
+
+    <!-- Default CSS -->
+    <link rel="stylesheet" href="./public/styles/default.css" />
+
+    <!-- Bulma -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css" />
+
     <title>Ciência de Dados - FATEC</title>
 </head>
 <body>
-    Teste
+    
 </body>
 </html>
